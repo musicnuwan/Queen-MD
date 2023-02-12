b3d5c863e34c4aeaff7bcaa52b3f9e92660ade87
musicnuwan <122086366+musicnuwan@users.noreply.github.com>
2023-02-12 12:39:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'eGNEY1hqVXo=',
+global.ownername = process.env.OWNER_NAME === undefined ? "𝗙𝗥𝗢𝗙𝗘𝗦𝗦𝗢𝗥..✨🎼" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '94786917462' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
